950ee80f984c4340b3bd9f2b900a52217ce3446a
Michael Williams <122236298+mikewilliams287@users.noreply.github.com>
2024-01-15 15:24:01 -0800

    Update scripting to trigger effects

--- Assets/Scripts/ThemeFoundBehavior.cs
+++ Assets/Scripts/ThemeFoundBehavior.cs
@@ -13,30 +13,84 @@ public class ThemeFoundBehavior : MonoBehaviour
     [SerializeField]
     private GameObject status;
 
+    [SerializeField]
+    private GameObject ThemePanel;
+
+    [SerializeField]
+    private GameObject Background;
+
+    Material GradientMaterial;
+    bool isFading = false;
+    bool hasPlayed = false;
+    float fade = 1f;
     private string newText;
 
+    private void SetDefaults() {
+        GradientMaterial.SetFloat("_Fade", fade);
+        confetti.SetActive(false);
+        newText = GetComponent<Text>().text;
+        status.SetActive(false);
+        ThemePanel.SetActive(false);
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
-        confetti.SetActive(false);
-        newText = GetComponent<Text>().text;
-        Debug.Log(newText);
-        status.SetActive(false);
+        // Get a reference to the material
+        GradientMaterial = Background.GetComponent<SpriteRenderer>().material;
 
+        SetDefaults();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //Click to toggle on/off partilces
+
         if (Input.GetMouseButtonDown(0))
         {
-            status.SetActive(!status.activeSelf);
-            confetti.SetActive(!confetti.activeSelf);
-            status.GetComponent<TMP_Text>().SetText(newText);
+            if (hasPlayed) {
+                SetDefaults();
+                hasPlayed = false;
+            } else {
+                //turn on or off the status text
+                status.SetActive(!status.activeSelf);
+
+                //turn on or off the particles
+                confetti.SetActive(!confetti.activeSelf);
+
+                //set the status text to the new text
+                status.GetComponent<TMP_Text>().SetText(newText);
+
+                //turn the theme panel on
+                ThemePanel.SetActive(!ThemePanel.activeSelf);
+                isFading = !isFading;
+                hasPlayed = true;
+
+                
+            }
+
+            
         }
 
+        if (isFading)
+        {
+            fade -= Time.deltaTime * 3;
+            
+            if (fade <= 0f)
+            {
+                fade = 0f;
+                isFading = false;
+            }
+
+            Debug.Log(fade);
+
+            //set the property
+            GradientMaterial.SetFloat("_Fade", fade);
+            
+        } else {
+            fade = 1f;
+        }
     }
+    
 
 }
